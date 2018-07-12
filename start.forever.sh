6788a894803305c298d0b72a89b09b151e62deec
franck <franck@diabeloop.fr>
2018-07-12 16:27:44 +0200

    Add append for log files

--- start.forever.sh
+++ start.forever.sh
@@ -10,4 +10,4 @@ nvm use --delete-prefix "${START_NODE_VERSION}"
 
 . config/env.sh
 
-forever -o ../server.log -e ../error.log -l ../forever.$service.log --uid $service start server.js
+forever -a -o ../$service.log -e ../$service.error.log --uid $service start server.js
